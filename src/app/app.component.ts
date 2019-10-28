121037f68ffaba1cd5fc2642a971f027522ab5aa
Chris Frolik <cfrolik@beckman.com>
2019-10-28 15:05:06 -0400

    added example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    this works if you see a picture of a cat *and* no errors in the console
+    <div class="cat" [style.background-image]="iconSafe" (click)="clickMe()"></div>
+  `,
+  styles: [`
+    .cat {
+      width: 200px;
+      height: 200px;
+      background-size: 200px 200px;
+      background-repeat: no-repeat;
+      background-position: 1px 1px;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'angular-ivy-test2';
+  icon = 'https://i.imgur.com/4AiXzf8.jpg';
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  get iconSafe() {
+    return this.sanitizer.bypassSecurityTrustStyle(`url(${this.icon})`);
+  }
+
+  clickMe() {
+
+  }
 }
